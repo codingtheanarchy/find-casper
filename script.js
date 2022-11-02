5812ba0abd05627deb0b598cc16a3436db6dd248
Honeypot <eric@thezombiecoder.com>
2022-11-01 18:37:12 -0700

    first commit

---
+++ script.js
@@ -0,0 +1,4 @@
+// TODO: When user opens door to scary ghost add 10 to progress bar
+// TODO: Create a randomized array of 10 url strings containing "casper.jpeg", "scary-ghost-1.jpg",
+//       and "scary-ghost-2.jpg", then map the indexes to the order the door elements display in html 
+// TODO: Inject text content into h2 within the results container
